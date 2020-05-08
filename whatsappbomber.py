5384f7294287d9429c8dab153b02cfa1f191f102
manan <mrpandya2017@gmail.com>
2020-05-09 02:37:05 +0530

    basic changes

--- whatsappbomber.py
+++ whatsappbomber.py
@@ -4,15 +4,16 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys 
 from selenium.webdriver.common.by import By 
 import time 
+target=input('enter the contact name: ')
+n=int(input('enter the number of messages you want to send: '))
+string=input('enter the message you want to send: ')
 driver = webdriver.Chrome('/home/manan/Desktop/fithit/chromedriver') 
 driver.get("https://web.whatsapp.com/") 
 wait = WebDriverWait(driver, 600) 
-target = '"Garam masala"' 
-string = "garam masala forever"
 x_arg = '//span[contains(@title,' + target + ')]'
 group_title = wait.until(EC.presence_of_element_located((By.XPATH, x_arg))) 
-group_title.click() 
+group_title.click()
 
-for i in range(200):
+for i in range(n):
     element=driver.find_element_by_css_selector("div[data-tab='1']")
     element.send_keys(string+Keys.ENTER)
