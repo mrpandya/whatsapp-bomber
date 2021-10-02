bd753c6f54e4cacb4bc898d58a10457ff7b31376
Pathik Prashant Ghugare <55437218+pathikg@users.noreply.github.com>
2021-10-02 20:04:03 +0530

    added try except block to handle exception of invalid path

--- whatsappbomber.py
+++ whatsappbomber.py
@@ -9,18 +9,27 @@ import time
 target=input('enter the contact name: ')
 n=int(input('enter the number of messages you want to send: '))
 string=input('enter the message you want to send: ')
-# path = Path of your chromedriver file
-driver = webdriver.Chrome('<path>')
-driver.get("https://web.whatsapp.com/")
-# wait for 6 seconds for whatsapp web to load and for you to login
-wait = WebDriverWait(driver, 600)
-# Check for the contact name in the title attribute of the span on the given page
-x_arg = '//span[contains(@title,\"' + target + '\")]'
-group_title = wait.until(EC.presence_of_element_located((By.XPATH, x_arg))) 
-# if the contact is found in the title attribute then it will select it and open the chats
-group_title.click()
-# Send the message for the number of times you have specified
-for i in range(n):
-    # select the send button which it searches using the CSS attribute
-    element=driver.find_element_by_css_selector("div[data-tab='1']")
-    element.send_keys(string+Keys.ENTER)
+
+flag = 0 
+try : 
+    # path = Path of your chromedriver file
+    driver = webdriver.Chrome('<path>')
+    flag = 1
+except : 
+    print("Invalid Path")
+
+if flag == 1 :
+    driver.get("https://web.whatsapp.com/")
+    # wait for 6 seconds for whatsapp web to load and for you to login
+    wait = WebDriverWait(driver, 600)
+    # Check for the contact name in the title attribute of the span on the given page
+    x_arg = '//span[contains(@title,\"' + target + '\")]'
+    group_title = wait.until(EC.presence_of_element_located((By.XPATH, x_arg))) 
+    # if the contact is found in the title attribute then it will select it and open the chats
+    group_title.click()
+    # Send the message for the number of times you have specified
+    for i in range(n):
+        # select the send button which it searches using the CSS attribute
+        element=driver.find_element_by_css_selector("div[data-tab='1']")
+        element.send_keys(string+Keys.ENTER)
+
